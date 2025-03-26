14e7e0e31ebe4397645fe0bd16f086e25bfad97e
Aleixs-Trejo <66097331+Aleixs-Trejo@users.noreply.github.com>
2025-03-26 12:11:05 -0500

    fixed letters, reset and colors

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,53 +4,20 @@ import "./../css/Board.css"
 // Components
 import Letter from "./Letter";
 
-// Context
+// Array
+const ROWS = 6;
+const COLUMNS = 5;
 
 const Board: React.FC = () => {
   return(
     <section className="section__board">
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={0} />
-        <Letter letterPos={1} attempVal={0} />
-        <Letter letterPos={2} attempVal={0} />
-        <Letter letterPos={3} attempVal={0} />
-        <Letter letterPos={4} attempVal={0} />
-      </article>
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={1} />
-        <Letter letterPos={1} attempVal={1} />
-        <Letter letterPos={2} attempVal={1} />
-        <Letter letterPos={3} attempVal={1} />
-        <Letter letterPos={4} attempVal={1} />
-      </article>
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={2} />
-        <Letter letterPos={1} attempVal={2} />
-        <Letter letterPos={2} attempVal={2} />
-        <Letter letterPos={3} attempVal={2} />
-        <Letter letterPos={4} attempVal={2} />
-      </article>
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={3} />
-        <Letter letterPos={1} attempVal={3} />
-        <Letter letterPos={2} attempVal={3} />
-        <Letter letterPos={3} attempVal={3} />
-        <Letter letterPos={4} attempVal={3} />
-      </article>
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={4} />
-        <Letter letterPos={1} attempVal={4} />
-        <Letter letterPos={2} attempVal={4} />
-        <Letter letterPos={3} attempVal={4} />
-        <Letter letterPos={4} attempVal={4} />
-      </article>
-      <article className="board__row">
-        <Letter letterPos={0} attempVal={5} />
-        <Letter letterPos={1} attempVal={5} />
-        <Letter letterPos={2} attempVal={5} />
-        <Letter letterPos={3} attempVal={5} />
-        <Letter letterPos={4} attempVal={5} />
-      </article>
+      {Array.from({ length: ROWS }).map((_, rowId) => (
+        <article key={rowId} className="board__row">
+          {Array.from({ length: COLUMNS }).map((_, colId) => (
+            <Letter key={colId} letterPos={colId} attempVal={rowId} />
+          ))}
+        </article>
+      ))}
     </section>
   )
 };
